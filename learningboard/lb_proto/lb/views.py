dc07c69121102a9cedb8d0fc34b8b1ae6d1c322c
Marco129 <marco.website01@gmail.com>
2016-06-21 15:56:38 +0800

    handle cors with middleware

--- learningboard/lb_proto/lb/views.py
+++ learningboard/lb_proto/lb/views.py
@@ -4,18 +4,13 @@ from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
-def Response(*args, **kwargs):
-    res = HttpResponse(*args, **kwargs)
-    res['Access-Control-Allow-Origin'] = '*'
-    return res
-
 def method_required(required_method):
     ''' returns a decorator specifing the method required for a request '''
     # parameters of the decorator
     def _decorator(func):
         def _wrapper(request, *args, **kwargs):
             if request.method != required_method.upper():
-                return Response(requrired_method+' request is required')
+                return HttpResponse(required_method + ' request is required')
             else:
                 return func(request, *args, **kwargs)
         return _wrapper
@@ -23,13 +18,13 @@ def method_required(required_method):
 
 @csrf_exempt
 def account_add(request):
-    return Response("done");
+    return HttpResponse("done");
 
 @csrf_exempt
 def lb_add(request):
-    return Response("done");
+    return HttpResponse("done");
 
 @csrf_exempt
 def activity_add(request):
-    return Response("done");
+    return HttpResponse("done");
 
