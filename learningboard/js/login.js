43dc108998b84c42633600d0bd376e968fe3fca8
Marco129 <marco.website01@gmail.com>
2016-06-23 19:33:44 +0800

    Store login session on local

--- learningboard/js/login.js
+++ learningboard/js/login.js
@@ -1,15 +1,21 @@
 
 $(document).ready(function()
 {
+  // redirect if logged in
+  if(localStorage['user_id']){
+    location.href = 'index.html';
+  }
   console.log($("button.loginBtn"));
 
   $("button.loginBtn").on("click", function(e) {
     e.preventDefault();
     var o = $("form.loginForm").serializeObject();
     console.log(o);
-    $.get(serv_addr+'accts/login/', o, function(data) 
+    $.get(serv_addr+'accts/login/', o, function(data)
     {
+      localStorage['user_id'] = data.pk;
       console.log(data.pk);
+      location.href = 'index.html';
     });
   });
 
