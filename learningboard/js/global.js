694f3999371a0b134a2eadf63ae1dcb56e985e1a
Marco129 <marco.website01@gmail.com>
2016-06-25 15:55:12 +0800

    Use js dump nav bar

--- learningboard/js/global.js
+++ learningboard/js/global.js
@@ -13,4 +13,43 @@ $(document).ready(function(){
     localStorage.removeItem('user_id');
     location.reload();
   });
+  // dump the nav bar to body
+  $('body').prepend(`
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.html">Learning Boards</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav navbar-right text-center">
+            <li><a href="#" class="addBoardBtn">
+              <span class="glyphicon glyphicon-floppy-disk" aria-hidden="true"></span>
+              <br />Save Board
+            </a></li>
+            <li><a href="#" class="deleteBoardBtn">
+              <span class="glyphicon glyphicon-remove-sign" aria-hidden="true"></span>
+              <br />Delete Board
+            </a></li>
+            <li><a href="#" class="publishBoardBtn">
+              <span class="glyphicon glyphicon-floppy-saved" aria-hidden="true"></span>
+              <br />Publish Board
+            </a></li>
+            <li class="hidden"><a href="#" class="unpublishBoardBtn">
+              <span class="glyphicon glyphicon-floppy-remove" aria-hidden="true"></span>
+              <br />Unpublish Board
+            </a></li>
+            <li><a href="#">
+              <span class="glyphicon glyphicon-modal-window" aria-hidden="true"></span>
+              <br />Preview Board
+            </a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>`);
 });
