08204e4f414d282d7d85f4e25480cee013cd6058
Marco129 <marco.website01@gmail.com>
2016-08-24 12:31:26 -0400

    Render news page

--- learningboard/js/news.js
+++ learningboard/js/news.js
@@ -1,28 +1,20 @@
-define(['util'], function(util)
+define(['util', 'temps/NewsListTemplate', 'temps/NewsTemplate'], function(util, NewsListTemplate, NewsTemplate)
 {
   $(function(){
     // fetch and render news
-    util.get('/news',
-      function(data)
+    util.get('/user/news',
+      function(res)
       {
-        if(data.news && data.news.length > 0)
-        {
-          $('div.row .noNews').hide();
-          for(var i = 0; i < data.news.length; i++)
-          {
-            $('div.row').append(`
-              <div class="col-md-12">
-                <h3>${data.news[i].title}</h3>
-                <p>
-                  Learning Board: <a href="board_view.html?${data.news[i].lb.id}" target="_blank">${data.news[i].lb.title}</a>
-                  |
-                  Author: ${data.news[i].lb.author}
-                </p>
-                <div>${data.news[i].text}</div>
-                <hr />
-              </div>`
-            );
-          }
+        var data = res.data.news;
+        if(data && data.length) {
+          data = data.map(function(item, i) {
+            return new NewsTemplate(item, i);
+          });
+          var list = new NewsListTemplate(data);
+          list.display($('#newsListContainer'));
+        } else {
+          var list = new NewsListTemplate();
+          list.display($('#newsListContainer'));
         }
       }
     );
